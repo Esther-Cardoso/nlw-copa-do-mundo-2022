6478985eb8af6de353cccd5ba15d4aa8519edb13
Esther Cardoso <esthercardosofernandes@gmail.com>
2022-11-13 14:10:04 -0300

    js finalizado

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function createGame() {
+function createGame(player1, nome1, hour, player2, nome2) {
   return `
     <li>
       <div>
-        <img src="./assets/icon-brazil.svg" alt="Bandeira do Brasil">
-        <h3>Brasil</h3>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <h3>${nome1}</h3>
       </div>
-      <strong>16:00</strong>
+      <strong>${hour}</strong>
       <div>
-        <img src="./assets/icon-serbia.svg" alt="Bandeira da Sérvia">
-        <h3>Sérvia</h3>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+        <h3>${nome2}</h3>
       </div>
     </li>
   `;
@@ -30,8 +30,21 @@ document.querySelector('#app').innerHTML = `
       <img src="./assets/logo.svg" alt="Logo NLW">
     </header>
     <main id="cards">
-      ${createCard('24/11', 'quinta', createGame())}
-      ${createCard('28/11', 'terça')}
-      ${createCard('02/12', 'sexta')}
+      ${createCard(
+        '24/11',
+        'quinta',
+        createGame('brasil', 'Brasil', '16:00', 'servia', 'Sérvia'),
+      )}
+      ${createCard(
+        '28/11',
+        'segunda',
+        createGame('brasil', 'Brasil', '13:00', 'suica', 'Suiça') +
+          createGame('portugal', 'Portugal', '16:00', 'uruguai', 'Uruguai'),
+      )}
+      ${createCard(
+        '02/12',
+        'sexta',
+        createGame('camaroes', 'Camarões', '16:00', 'brasil', 'Brasil'),
+      )}
     </main>
 `;
